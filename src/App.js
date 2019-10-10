ae5027d4c172d68f1f9560cc7156f29897c2aab6
Lincon Kusunoki <linconkusunoki@gmail.com>
2019-10-09 22:29:34 -0300

    Add Gnome Profile and global styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { createGlobalStyle } from 'styled-components'
 
-import { Home as HomeView, Detail as DetailView } from './views'
+import { Home as HomeView, Profile as ProfileView } from './views'
+
+const GlobalStyle = createGlobalStyle`
+  * { box-sizing: border-box }
+  body { margin: 0; min-height: 100vh; font-family: 'Source Sans Pro', sans-serif; }
+  img { max-width: 100% }
+  #root { background-color: #f2f5f7 }
+`
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={HomeView} />
-        <Route exact path="/gnome/:id" component={DetailView} />
-      </Switch>
-    </BrowserRouter>
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomeView} />
+          <Route exact path="/gnome/:id" component={ProfileView} />
+        </Switch>
+      </BrowserRouter>
+    </>
   )
 }
 
