49926299c06de443f6c4868377db14e3306f2e5b
Lincon Kusunoki <linconkusunoki@gmail.com>
2019-10-09 22:26:47 -0300

    Add filter by ID to gnomes service

--- src/services/gnomes.js
+++ src/services/gnomes.js
@@ -1,7 +1,13 @@
 import api from './api'
 
-const get = async () => {
+const get = async id => {
   const res = await api.get('/rrafols/mobile_test/master/data.json')
+
+  if (id) {
+    const gnome = res.data.Brastlewark.find(gnome => gnome.id === parseInt(id))
+    return gnome
+  }
+
   return res.data.Brastlewark
 }
 
