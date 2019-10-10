cfc41a3e00463a349ae6e2400f43d16165c0771f
Lincon Kusunoki <linconkusunoki@gmail.com>
2019-10-09 22:25:44 -0300

    Rename detail to profile

---
+++ src/views/Profile/Profile.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Layout from '../../layout'
+import { GnomeProfile } from '../../components'
+
+const Profile = ({ match }) => {
+  const { id } = match.params
+  return (
+    <Layout.Wrapper>
+      <Layout.Header title="Profile" />
+      <Layout.Main>
+        <GnomeProfile id={id} />
+      </Layout.Main>
+    </Layout.Wrapper>
+  )
+}
+
+export default Profile
