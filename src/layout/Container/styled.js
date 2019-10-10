f4a5718ed4471a4e3e379fc81bb3179479251b58
Lincon Kusunoki <linconkusunoki@gmail.com>
2019-10-09 22:27:51 -0300

    Add Container layout

---
+++ src/layout/Container/styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 1024px;
+  position: relative;
+  padding: 0 1rem 1rem;
+`
--- src/layout/index.js
+++ src/layout/index.js
@@ -1,9 +1,11 @@
 import Header from './Header'
 import Main from './Main'
 import Wrapper from './Wrapper'
+import Footer from './Footer'
 
 export default {
   Header,
   Wrapper,
   Main,
+  Footer,
 }
